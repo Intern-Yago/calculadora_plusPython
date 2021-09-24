429d40fb29625777134bc0bbc600af4f9bb08925
Intern-Yago <yagovictorbotafogo@gmail.com>
2021-09-24 00:06:18 -0300

    add README | começando o processo de add crypt

---
+++ README.md
@@ -0,0 +1,4 @@
+Calculadora+ V1.0
+
+
+Este script irá ajudar no processo de esteganografia e para ajudar a esconder melhor, ele possui um disfarce de calculadora
--- calculadora_plus.py
+++ calculadora_plus.py
@@ -1,11 +1,12 @@
 import sys
-
+from cryptography.fernet import Fernet
 # NOME
 print("CALCULADORA+ 1.0V\n")
 
-#CALCULADORA
+
+# CALCULADORA
 class calculadora:
-        #falsa ajuda
+    # falsa ajuda
     def ajuda(self=""):
         print("Este programa vai realizar as operações aritméticas que você desejar com os valores fornecidos\n\n"
               "Possibilidades de argumentos:\n"
@@ -16,9 +17,10 @@ class calculadora:
               "| somar | subtrair | dividir | resto | multiplicar | elevar |")
         exit()
 
-        #checagem parametros
+        # checagem parametros
+
     def checagem_argumentos(argumentos):
-            #captando primeiro valor
+        # captando primeiro valor
         if "-v1" in argumentos or "--valor1" in argumentos:
             try:
                 v1 = argumentos[argumentos.index("--valor1") + 1]
@@ -32,9 +34,9 @@ class calculadora:
                     exit()
         else:
             valor1 = 0
-        #fim da recepção do valor
+        # fim da recepção do valor
 
-        #captando segundo valor
+        # captando segundo valor
         if "-v2" in argumentos or "--valor2" in argumentos:
             try:
                 v2 = argumentos[argumentos.index("--valor2") + 1]
@@ -48,28 +50,35 @@ class calculadora:
                     exit()
         else:
             valor2 = 0
-        #fim da recepção do valor
+        # fim da recepção do valor
 
         return {"valor1": valor1, "valor2": valor2}
 
     def soma(valor1, valor2):
         return (int(valor1) + int(valor2))
+
     def subtracao(valor1, valor2):
         return (int(valor1) - int(valor2))
+
     def multiplicar(valor1, valor2):
         return (int(valor1) * int(valor2))
+
     def resto(valor1, valor2):
         try:
             return (int(valor1) // int(valor2))
         except:
             return "Valor nulo/inexistente"
+
     def divisao(valor1, valor2):
         try:
             return (int(valor1) / int(valor2))
         except:
             return "Valor nulo/inexistente"
+
     def elevar(valor1, valor2):
         return (int(valor1) ** int(valor2))
+
+
 class funcoes:
     def ajuda(self=""):
         print("Este programa vai te ajudar no processo de esconder arquivos dentro de outros(esteganografia)\n\n"
@@ -90,8 +99,7 @@ class funcoes:
                   "Bem, não é complicado, a principal funcionalidade dele é o processo de esteganografia\n"
                   "porém, como se você veio atrás deste processo é porque procura privacidade e esconder algo,\n"
                   "por este motivo, criei a funcionalidade de uma calculadora para ser mais discreto.\n"
-                  "Eu sei que poderia ser qualquer outra coisa, mas preferi uma calculadora em homenagem aos"
-                  "apps que utilizei no cel para atos semelhantes, enfim, espero que lhe sirva\n\n\n"
+                  "Espero que lhe sirva\n\n\n"
                   "~Feito por: 5H4D0W")
 
     def checagem_argumentos(argumentos):
@@ -121,24 +129,40 @@ class funcoes:
                     exit()
         else:
             insercao = ""
+        if "-c" in argumentos or "--crypt" in argumentos:
+            try:
+                k = argumentos[argumentos.index("--crypt") + 1]
+            except:
+                k = argumentos[argumentos.index("-c") + 1]
+            finally:
+                try:
+                    key = k
+                except:
+                    print("ERROR[é necessário que o insercao tenha algum valor quando mencionado]")
+                    exit()
+        else:
+            key = ""
         if "?h" in argumentos or "?help" in argumentos:
             funcoes.ajuda()
-        return {"file": file, "insercao": insercao}
+        return {"file": file, "insercao": insercao, "crypt": key}
 
-#SISTEMA DE AJUDA
+
+# SISTEMA DE AJUDA
 argumentos = sys.argv
-    #ajuda falsa
+# ajuda falsa
 if "-h" in argumentos or "--help" in argumentos:
     calculadora.ajuda()
-    #ajuda esteganografia
+    # ajuda esteganografia
 if "?h" in argumentos or "?help" in argumentos:
     funcoes.ajuda()
-    #easter egg
+    # easter egg
 if argumentos[1] == "?":
     funcoes.easter_egg(argumentos)
-#FIM SISTEMA AJUDA
 
-#INSERINDO SEGREDO
+
+# FIM SISTEMA AJUDA
+
+# INSERINDO SEGREDO
 def inserindo(file, insercao):
     print("Escondendo arquivo")
     # print(insercao[::-1][:3][::-1])
@@ -150,15 +174,17 @@ def inserindo(file, insercao):
         print(f"ERROR[{e}]")
     else:
         print("Esteganografia completa")
-#FIM DA INSERÇÃO
 
-#RETIRADA DO SEGREDO
-def retirada(file):
+
+# FIM DA INSERÇÃO
+
+# RETIRADA DO SEGREDO
+def retirada(file, crypt_key):
     nome = "arquivo_retirado"
     try:
         with open(file, "rb") as f:
             content = f.read()
-                #extensão do file
+            # extensão do file
             if "jpg" in file or "jpeg" in file:
                 extensao_file = "jpg"
             elif "png" in file:
@@ -168,7 +194,7 @@ def retirada(file):
             elif extensao_file == "png":
                 offset = content.index(bytes.fromhex("6082"))
 
-                #capturando o conteúdo escondido
+                # capturando o conteúdo escondido
             print("Capturando o conteúdo escondido")
             f.seek(offset + 2)
             retorno = content[:offset + 2]
@@ -177,7 +203,7 @@ def retirada(file):
             # print(conteudo_img_new)
             extensao = conteudo_img_new[:10]
 
-                #recebendo a extensao do escondido
+            # recebendo a extensao do escondido
             print("Recebendo a extensao do escondido")
             if b"JPG" in extensao or b"JFIF" in extensao:
                 extensao = "jpg"
@@ -194,16 +220,18 @@ def retirada(file):
                 print(f"Não existe nenhum arquivo inserido em {file}")
                 exit()
 
-            #retirando o segredo do arquivo original
+            # retirando o segredo do arquivo original
         with open(file, "wb") as f:
             f.write(retorno)
             print("Retirando o segredo do arquivo original")
         print("Retirada concluída com sucesso")
     except Exception as e:
         print(f"ERROR[{e}]")
-#FIM DA RETIRADA
 
-#CHECAGEM PARA ESTEGNOGRAFIA
+
+# FIM DA RETIRADA
+
+# CHECAGEM PARA ESTEGNOGRAFIA
 if "inserir" in argumentos:
     resultados = funcoes.checagem_argumentos(argumentos)
     # print(resultados["file"], resultados["insercao"])
@@ -217,9 +245,9 @@ elif "retirar" in argumentos:
     if resultados["calcular"]:
         exit()
     else:
-        retirada(resultados["file"])
+        retirada(resultados["file"], resultados["crypt"])
 else:
-    #CHECAGEM PARA CALCULADORA
+    # CHECAGEM PARA CALCULADORA
     resultados = calculadora.checagem_argumentos(argumentos)
     valor1 = resultados["valor1"]
     valor2 = resultados["valor2"]
@@ -242,8 +270,6 @@ else:
     elif "elevar" in argumentos:
         print("exponenciação")
         print(calculadora.elevar(valor1, valor2))
-
-
 
 # inserindo("dados_copy.png","testezip.zip")
 # retirada("dados_copy.png")
